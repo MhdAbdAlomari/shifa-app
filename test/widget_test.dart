b5885bd0b3e73cf6000b9f2bd67abfa63fbf0f18
M-Abdulrhman-Alomari <abdulrhmangithub@gmail.com>
2026-09-09 00:40:04 +0300

    test: add placeholder widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('placeholder', () {
+    // Widget tests will be added per screen. This file exists so
+    // `flutter test` has at least one target.
+    expect(1 + 1, 2);
+  });
+}
